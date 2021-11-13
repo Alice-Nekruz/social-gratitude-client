6f01febf456d3f1f95212caddf6d97d4894afa23
Alice <alice.jost@outlook.de>
2021-11-13 14:23:30 +0100

    create register componenet

---
+++ src/components/auth/Register.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import authService from "../../services/auth-services";
+
+class Signup extends Component {
+
+    state = { username: '', password: '' }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const { username, password } = this.state;
+
+        authService.signup(username, password)
+            .then(createdUser => {
+                this.setState({
+                    username: "",
+                    password: "",
+                });
+                // this.props.getUser(response, true);
+            })
+            .catch(error => console.log(error))
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>
+                        Username:
+                        <input
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+
+                    <button type="submit"> Signup </button>
+                </form>
+
+                <p>
+                    Already have an account?
+                    <Link to={"/"}> Login</Link>
+                </p>
+
+            </div>
+        )
+    }
+
+}
+
+export default Signup;
