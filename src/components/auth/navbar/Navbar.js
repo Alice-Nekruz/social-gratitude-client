45ab40644470407e6bb1115bc0363467b32522db
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-11-17 23:16:42 +0000

    Navbar is styled

--- src/components/auth/navbar/Navbar.js
+++ src/components/auth/navbar/Navbar.js
@@ -3,6 +3,9 @@ import './style/navbar.css';
 import { Link } from 'react-router-dom';
 import authService from '../../../services/auth-services';
 
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export default class Navbar extends Component {
     logoutUser = () => {
         authService.logout().then(() => {
@@ -16,12 +19,13 @@ export default class Navbar extends Component {
         if (isLoggedIn) {
             return (
                 <nav className="nav-style">
-                        <div className="logo"><Link to={'/posts'}> GRATITUDE</Link></div>
-                        <div>Welcome, {userData.username}</div>
-                        <div>
-                            <Link to="/">
-                                <button onClick={() => this.logoutUser()}>Logout</button>
+                        <div className="logo"><Link className="logo-text" to={'/posts'}> GRATITUDE</Link></div>
+                        <div className="welcome">Welcome, {userData.username}</div>
+                        <div >
+                            <Link className="logout-button" to="/">
+                                <Button onClick={() => this.logoutUser()} variant="contained" color="secondary">Logout</Button>
                             </Link>
+                            
                         </div>
                 </nav>
             );
