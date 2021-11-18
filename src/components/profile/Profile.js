364a68a62d5665b203815e94f7f1d6fc1412bcd3
Alice <alice.jost@outlook.de>
2021-11-18 17:54:46 +0100

    conditional rendering on the welcome msg

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -48,7 +48,10 @@ class Profile extends React.Component {
     render() {
         return (
             <div>
-                <h1>Welcome to your profile</h1>
+                {this.state.user._id === this.props.getUser?._id ?
+                    <h1>Welcome to your profile</h1>
+                    : <h1> This is the profile of</h1>
+                }
                 <h1>{this.state.user.username}</h1>
                 <h1>{this.state.user.name}</h1>
                 <h1>{this.state.user.gender}</h1>
